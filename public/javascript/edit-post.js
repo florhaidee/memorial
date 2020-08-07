73f51f962231e1f2df1c0ecb827f4d86dcef4381
John Nielsen <johnhayesnielsen@gmail.com>
2020-08-06 20:47:39 -0600

    added code for views and public

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,30 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const content = document.querySelector('textarea[name="post-body"]').value;
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    };
+
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
