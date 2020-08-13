6ae8cc4f6249560bd184f9d78bf1b37f7a1f539a
Florhaidee <florhaideedev@gmail.com>
2020-08-13 15:28:30 -0600

    fixed delete a post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -145,4 +145,64 @@ router.post('/', withAuth, (req, res) => {
   })
 })
 
+router.put('/edit/:id', withAuth, (req, res) => {
+  upload(req, res, (err) => {
+    if (err){
+      res.status(500).json(err);
+    } else {
+      if(req.file!== 'undefined'){
+        Post.update({
+          title: req.body.title,
+          birthDate: req.body.birthDate,
+          passingDate: req.body.pasingDate,
+          avatar: `images/${req.file.filename}`,
+          content: req.body.body,  
+          user_id: req.session.user_id
+        })
+        .then(response => {
+          Post.findAll({
+            where: {
+              // use the ID from the session
+              user_id: req.session.user_id
+            },
+            attributes: [
+              'id',
+              'title',
+              'birthDate',
+              'passingDate',
+              'content',
+              'avatar',
+              'created_at',
+            ],
+            include: [
+              {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                  model: User,
+                  attributes: ['username']
+                }
+              },
+              {
+                model: User,
+                attributes: ['username']
+              }
+            ]
+          })
+          .then(dbPostData => {
+            // serialize data before passing to template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            //res.render('dashboard', { posts, loggedIn: true });
+            res.redirect("/dashboard")
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+        }
+      )}
+    }
+  })
+})
+
 module.exports = router;
