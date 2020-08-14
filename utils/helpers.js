a2110737860f0ef55f2c2b02116285b16925e4ac
Florhaidee <florhaideedev@gmail.com>
2020-08-14 15:03:05 -0600

    just cleanup the images folder

--- utils/helpers.js
+++ utils/helpers.js
@@ -12,12 +12,4 @@ module.exports = {
     
         return word;
     },
-//     format_url: url => {
-//         return url
-//         .replace('http://', '')
-//         .replace('https://', '')
-//         .replace('www.', '')
-//         .split('/')[0]
-//         .split('?')[0];
-//     },
   }
